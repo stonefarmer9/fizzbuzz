baa183a64f01ca1cb4d2fd7942c26110259e26af
Georges Le Vaillant <g.p.levaillant@gmail.com>
2018-11-23 17:52:39 +0000

    3rd test - Returns FizzBuzz when passed 15

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,7 +1,9 @@
 def fizzbuzz(number)
-   if number % 3 == 0 
-    "fizz"
+    if number % 3 == 0 && number % 5 == 0
+      "FizzBuzz"
+    elsif number % 3 == 0 
+      "fizz"
     elsif number % 5 == 0
       "buzz"
-   end 
+    end 
 end 
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -7,5 +7,9 @@ describe 'fizzbuzz' do
    it 'returns "buzz" when passed 5' do
       expect(fizzbuzz(5)).to eq 'buzz'
   end
+  
+  it 'returns "FizzBuzz" when passed 15' do
+    expect(fizzbuzz(15)).to eq "FizzBuzz"
+  end  
 end  
 
