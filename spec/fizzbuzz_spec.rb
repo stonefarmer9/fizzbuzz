16cf327c057e1a7fb6ce2e3bc2a5fecfb57a7684
Georges Le Vaillant <g.p.levaillant@gmail.com>
2018-11-23 18:13:28 +0000

    Added 7 more tests, testing numbers that are divisible by 3, divsible by 5 and neither - All tests pass and code refactored

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -15,4 +15,32 @@ describe 'fizzbuzz' do
   it 'returns the number when a number does not divide by 3 or 5' do
     expect(fizzbuzz(7)).to eq 7
   end 
+  
+   it 'returns the number when a number does not divide by 3 or 5' do
+    expect(fizzbuzz(58)).to eq 58
+  end 
+  
+   it 'returns the number when a number does not divide by 3 or 5' do
+    expect(fizzbuzz(67)).to eq 67
+  end 
+  
+  it 'returns "buzz" when passed number divisible by 5' do
+      expect(fizzbuzz(5)).to eq 'buzz'
+  end
+  
+  it 'returns "buzz" when passed number divisible by 5' do
+      expect(fizzbuzz(20)).to eq 'buzz'
+  end
+  
+  it 'returns "buzz" when passed number divisible by 5' do
+      expect(fizzbuzz(95)).to eq 'buzz'
+  end
+  
+  it 'returns "fizz" when passed a number divisible by 3' do
+    expect(fizzbuzz(9)).to eq 'fizz'
+  end
+  
+  it 'returns "fizz" when passed a number divisible by 3' do
+    expect(fizzbuzz(69)).to eq 'fizz'
+  end
 end  
